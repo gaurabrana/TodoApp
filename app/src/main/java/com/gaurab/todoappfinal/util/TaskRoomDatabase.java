6ea2a42f026eeb69b742bdcb0b8ce4cec2949b34
Gaurab Rana <ericwinty90@gmail.com>
2021-03-31 04:42:57 +0545

    Database Implemented

---
+++ app/src/main/java/com/gaurab/todoappfinal/util/TaskRoomDatabase.java
@@ -0,0 +1,60 @@
+package com.gaurab.todoappfinal.util;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.gaurab.todoappfinal.model.Task;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Task.class}, version = 1, exportSchema = false)
+@TypeConverters(DateConverter.class)
+public  abstract class TaskRoomDatabase extends RoomDatabase {
+
+    public  static TaskRoomDatabase INSTANCE;
+
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+
+    public static TaskRoomDatabase getDatabase(Context context){
+        if(INSTANCE == null){
+            synchronized (TaskRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskRoomDatabase.class, "todo.db")
+                            //.allowMainThreadQueries()
+                            .addCallback(CALLBACK)
+                            .build();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback CALLBACK = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            TaskRoomDatabase.databaseWriteExecutor.execute(() -> {
+
+            });
+
+        }
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+        }
+    };
+}
